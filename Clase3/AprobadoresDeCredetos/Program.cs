c2877fabad057411f9bdafa0731d38eea9e7ce75
Ariel <arielsrv@gmail.com>
2018-11-09 20:28:33 -0300

    Agrego Iterator + Chain of responsibility.

---
+++ Clase3/AprobadoresDeCredetos/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprobadoresDeCreditos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount = 500;
+
+            Approver employee = new Employee();
+            Approver supervisor = new Supervisor();
+            Approver manager = new Manager();
+            Approver director = new Director();
+
+            employee.SetNext(supervisor);
+            supervisor.SetNext(manager);
+            manager.SetNext(director);
+
+            employee.HandleRequest(amount);
+        }
+    }
+
+    abstract class Approver
+    {
+        protected Approver next;
+
+        public void SetNext(Approver next)
+        {
+            this.next = next;
+        }
+
+        public abstract void HandleRequest(double amount);
+    }
+
+    class Employee : Approver
+    {
+        public override void HandleRequest(double amount)
+        {
+            if ((amount > 0) && (amount < 1000))
+            {
+                Console.WriteLine($"Approve by {this.GetType().Name}");
+            }
+            else
+            {
+                this.next.HandleRequest(amount);
+            }
+        }
+    }
+
+    class Supervisor : Approver
+    {
+        public override void HandleRequest(double amount)
+        {
+            if ((amount > 1000) && (amount < 10000))
+            {
+                Console.WriteLine($"Approve by {this.GetType().Name}");
+            }
+            else
+            {
+                this.next.HandleRequest(amount);
+            }
+        }
+    }
+
+    class Manager : Approver
+    {
+        public override void HandleRequest(double amount)
+        {
+            if ((amount > 10000) && (amount < 500000))
+            {
+                Console.WriteLine($"Approve by {this.GetType().Name}");
+            }
+            else
+            {
+                this.next.HandleRequest(amount);
+            }
+        }
+    }
+
+    class Director : Approver
+    {
+        public override void HandleRequest(double amount)
+        {
+            if (amount > 500000)
+            {
+                Console.WriteLine($"Approve by {this.GetType().Name}");
+            }
+        }
+    }
+
+}
---
+++ Clase3/Colecciones/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICollection<int> values = new List<int>();
+            values.Add(1);
+            values.Add(4);
+            values.Add(0);
+            values.Add(3);
+
+            IEnumerator<int> enumerator = values.GetEnumerator();
+            enumerator.Reset();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+
+    class MyCollection<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
