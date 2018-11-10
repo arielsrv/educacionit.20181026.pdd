aa7bb77de19bd2ba32a920c0b4b551f528c29176
Ariel <arielsrv@gmail.com>
2018-11-09 21:53:06 -0300

    Agrego abstract factory.

---
+++ Clase3/Documentos/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documentos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Document resume = new Resume();
+
+            foreach (Page page in resume.GetPages())
+            {
+                Console.WriteLine(page.GetType().Name);
+            }
+
+        }
+    }
+
+    abstract class Document
+    {
+        protected List<Page> pages = new List<Page>();
+
+        public List<Page> GetPages()
+        {
+            return this.pages;
+        }
+
+        public Document()
+        {
+            CreatePages();
+        }
+
+        public abstract void CreatePages();
+    }
+
+    class Resume : Document
+    {
+        public override void CreatePages()
+        {
+            this.pages.Add(new Skill());
+            this.pages.Add(new AcademicalHistory());
+        }
+    }
+
+    class Invoice : Document
+    {
+        public override void CreatePages()
+        {
+            this.pages.Add(new Header());
+        }
+    }
+
+    class Page
+    {
+
+    }
+
+    class Skill : Page
+    {
+
+    }
+
+    class Header : Page
+    {
+
+    }
+
+    class AcademicalHistory : Page
+    {
+
+    }
+}
---
+++ Clase3/FamiliasDeDao/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamiliasDeDao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractDaoProvider abstractDao = new OracleDaoProvider();
+            ProductDao productDao = abstractDao.CreateProductDao();
+        }
+    }
+
+    abstract class AbstractDaoProvider
+    {
+        public abstract ProductDao CreateProductDao();
+        public abstract CustomerDao CreateCustomerDao();
+    }
+
+
+    class OracleDaoProvider : AbstractDaoProvider
+    {
+        public override CustomerDao CreateCustomerDao()
+        {
+            return new OracleCustomerDao();
+        }
+
+        public override ProductDao CreateProductDao()
+        {
+            return new OracleProductDao();
+        }
+    }
+
+    class SqlServerDaoProvider : AbstractDaoProvider
+    {
+        public override CustomerDao CreateCustomerDao()
+        {
+            return new SqlServerCustomerDao();
+        }
+
+        public override ProductDao CreateProductDao()
+        {
+            return new SqlServerProductDao();
+        }
+    }
+    
+    abstract class ProductDao
+    {
+
+    }
+
+    abstract class CustomerDao
+    {
+
+    }
+
+    class SqlServerProductDao : ProductDao
+    {
+
+    }
+
+    class OracleProductDao : ProductDao
+    {
+
+    }
+
+    class SqlServerCustomerDao : CustomerDao
+    {
+
+    }
+
+    class OracleCustomerDao : CustomerDao
+    {
+
+    }
+
+}
